c0cf8fe21974f9718b0cf29654a5b03dc1b8de0b
Nicole Sawler <nicolesawler@gmail.com>
2023-02-04 11:11:20 -0400

    Add files via upload

---
+++ glassceiling2/index.php
@@ -0,0 +1,268 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" class="holderjs">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+    <title>TheGlassCeiling.SUCKS</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <header class="header">
+        <div class="name">The Glass Ceiling <span class="pixel">.SUCKS</span></div>
+    </header>
+
+    <main role="main" class="bg-cream">
+
+        <section class="jumbotron">
+
+            <div class="container opening">
+
+                <div class="col-md-6">
+                    <h1 class="top-headline">This ceiling begs to be smashed</h1>
+                    <p class="lead-headline">Glass may be transparent but we still know it’s there.</p>
+                    <p>Which is exactly why we use “the glass ceiling” to describe the unspoken, unwritten and unacknowledged limitations imposed upon women and minorities in their professional careers.
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                    </p>
+                </div>
+
+            </div>
+        </section>
+
+        <section class="call-boxes">
+            <img src="images/statsbg.png" class="statsbg" />
+
+            <div class="container ">
+
+                <div class="row ">
+
+                    <div class="col-md-4 call-boxes-single">
+                        <p class="call-boxes-single-text"><b>5.2%</b></p>
+                        <img src="images/stat.png" class="call-boxes-single-bg" />
+                        <div class="call-boxes-single push-top">
+                            Just <b>5.2%</b> of S&P 500 CEOs are <b>women</b>.
+                        </div>
+                    </div>
+                    <div class="col-md-4 call-boxes-single">
+                        <p class="call-boxes-single-text">20%</p>
+                        <img src="images/stat2.png" class="call-boxes-single-bg" />
+                        <div class="call-boxes-single push-top">
+                            Less than a <b>quarter (20.2%)</b> of Fortune 500 board representation is <b>female</b>.
+                        </div>
+                    </div>
+                    <div class="col-md-4 call-boxes-single">
+                        <p class="call-boxes-single-text">3.8%</p>
+                        <img src="images/stat.png" class="call-boxes-single-bg" />
+                        <div class="call-boxes-single push-top">
+                            Of that, only <b>3.8%</b> features <b>minorities</b>.
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="row ">
+
+                    <div class="col-md-12 ">
+                        <hr class="blueline" />
+                        <h3 class="red">It’s time we answer the rhetoric with action.</h3>
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+
+        <section class="">
+
+            <div class="container info-section-main">
+                <br>
+                <br>
+                <h2>Here’s how you can help smash through the glass ceiling</h2>
+
+                <div class="row">
+
+                    <div class="col-md-4 ">
+
+                        <div id="square"><span>1</span></div>
+                        <img src="images/woman1.png" class="thumb" />
+                        <div class="quote-box">
+                            <h2>As women in positions of power, hire your own. </h2>
+                            <p>Once you’ve worked your way to the top, extend the opportunity to your female peers. As a female executive or director, you have the power to bring more women into senior management roles. </p>
+                        </div>
+
+                    </div>
+
+                    <div class="col-md-4 ">
+
+                        <div id="square"><span>2</span></div>
+                        <img src="images/woman2.png" class="thumb" />
+                        <div class="quote-box" style="background-color:#f9f8f8;">
+                            <h2>As minorities in an organization, be loud about your wins. </h2>
+                            <p>You bring a unique perspective, so talk about it and celebrate it—especially with your supervisors. Your company benefits from your world view—and those above you should be reminded. </p>
+                        </div>
+
+                    </div>
+
+                    <div class="col-md-4 ">
+
+                        <div id="square"><span>3</span></div>
+                        <img src="images/woman3.png" class="thumb" />
+                        <div class="quote-box">
+                            <h2>As men with inherent advantage, actively challenge your personal biases.</h2>
+                            <p>Be conscious about giving your female colleagues the voice and recognition they deserve—for every male you sponsor within your organization, sponsor a female colleague as well.</p>
+                        </div>
+
+                    </div>
+
+                </div>
+                <br>
+                <br>
+
+                <div class="row">
+                    <h5>Each time you acknowledge women and minorities who are accomplishing incredible things, the ceiling gets weaker.</h5>
+                </div>
+            </div>
+
+        </section>
+     <section class="blog">
+
+            <div class="container info-section-main">
+                <br><br>
+                <h2>Stories that inspire</h2>
+
+                <div class="row">
+
+                    <div class="col-md-4">
+
+                        <div style="position:relative;">
+                            <div id="rectangle"><span>FROM THE BLOG</span></div>
+                            <img src="images/blog1.png" class="thumb" />
+                        </div>
+                        <div class="blog-box">
+                            <a href="https://www.get.sucks/blog/the-glass-ceiling-sucks/">The Glass Ceiling Sucks: 7 Women Killing It in Big Deal Jobs </a>
+                        </div>
+
+                    </div>
+
+                    <div class="col-md-4">
+
+                        
+                        <div style="position:relative;">
+                            <div id="rectangle"><span>FROM THE BLOG</span></div>
+                            <img src="images/blog2.png" class="thumb" />
+                        </div>
+                        
+                        <div class="blog-box">
+                           <a href="https://www.get.sucks/blog/5-real-wonder-women-ensuring-future-female/">5 Real Wonder Women Who Are Ensuring the Future Is Female</a>
+                        </div>
+
+                    </div>
+
+                    <div class="col-md-4">
+
+                        <div style="position:relative;">
+                            <div id="rectangle"><span>FROM THE BLOG</span></div>
+                            <img src="images/blog3.png" class="thumb" />
+                        </div>
+                        <div class="blog-box">
+                           <a href="https://www.get.sucks/blog/men-minorities-can-fight-feminism/">How Men and Minorities Can Fight for Feminism</a>
+                        </div>
+
+                    </div>
+
+                </div>
+                     <br>
+                <br>
+            </div>
+
+        </section>       
+        
+        
+        
+        
+        
+    </main>
+
+    <footer class="patternBgStriped DomainFooter">
+
+        <div class="container padded quote">
+
+            <h1>Smashing the glass ceiling might sound daunting at first—but perpetuating the gender gap sucks even more.</h1>
+
+        </div>
+
+    </footer>
+
+    <footer class="bluefooter">
+
+        <div class="container bluefooter">
+
+            <h2>What else sucks?</h2>
+            <p>Whether you are looking to market your brand in a unique way, or want to amplify the conversation about something that sucks, find your voice with a .SUCKS domain.</p>
+
+            <a href="http://get.sucks">
+                <button class="btn-exploreDomains">
+                    <i class="fas fa-angle-right"></i>&nbsp; Explore domains now
+                </button>
+            </a>
+
+            <br>
+            <p>&copy;
+                <?= date('Y') ?> Vox Populi Registry Ltd. All Rights Reserved.</p>
+
+        </div>
+
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/holder.min.js"></script>
+    <script>
+        jQuery('.ScrollToTop').animate({
+            scrollTop: 0
+        }, 0);
+
+        Holder.addTheme('thumb', {
+            bg: '#55595c',
+            fg: '#eceeef',
+            text: 'Thumbnail'
+        });
+    </script>
+</body>
+
+</html>
